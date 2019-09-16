96fbae07d94fadda6247c8d84051eb489c0e617c
redcountry15 <33802969+redcountry15@users.noreply.github.com>
2019-09-16 12:44:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myschooltexttopdf/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package com.example.myschooltexttopdf;
+
+import android.content.ContentProvider;
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+}
